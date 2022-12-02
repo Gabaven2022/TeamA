841fdcb217f4c9c4ab3e8bd47b7e77ffec15d4c1
Kazuki.Yamakawa <18236091@edu.cc.saga-u.ac.jp>
2022-12-02 18:20:40 +0900

    全フッター編集

---
+++ 試作1/置換君ネオ.py
@@ -0,0 +1,18 @@
+import glob
+import os
+
+for file_name in glob.glob("*.html"):
+    with open(file_name,encoding="UTF-8") as f:
+         data_lines=f.read()
+    
+    data_lines=data_lines.replace('フッターメニュー1','HOME')
+    data_lines=data_lines.replace('フッターメニュー2','GUIDELINE')
+    data_lines=data_lines.replace('フッターメニュー3','SERVICE')
+    data_lines=data_lines.replace('フッターメニュー4','FAO')
+    data_lines=data_lines.replace('フッターメニュー5','CONTACT')
+
+    with open(file_name, mode="w", encoding="UTF-8") as f:
+        f.write(data_lines)
+
+#文字置換君は通常は1つのファイルを指定して、そのファイル内の指定した文字を置換します。
+#置換君ネオは全ファイル内の指定された文字を置換します。
