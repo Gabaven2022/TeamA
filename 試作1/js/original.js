31f62464dd3d2378552ae11da37c0b230a69e735
Kazuki.Yamakawa <18236091@edu.cc.saga-u.ac.jp>
2022-12-04 02:21:12 +0900

    homeとserviceの編集（java,css追加）

--- 試作1/js/original.js
+++ 試作1/js/original.js
@@ -16,7 +16,8 @@ $(window).scroll(function () {
     var scrollAnimationElm = document.querySelectorAll('.scroll_up2');
     var scrollAnimationFunc = function () {
       for (var i = 0; i < scrollAnimationElm.length; i++) {
-        var triggerMargin = 20;
+        var triggerMargin = 15;
+//5pxだけ遅く表示
         if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
           scrollAnimationElm[i].classList.add('show');
         }
@@ -24,4 +25,41 @@ $(window).scroll(function () {
     }
     window.addEventListener('load', scrollAnimationFunc);
     window.addEventListener('scroll', scrollAnimationFunc);
+  });
+
+
+  // 背景色が伸びて出現（右から左）
+  function BgFadeAnime(){
+
+  $('.bgRLextendTrigger').each(function(){ 
+    var elemPos = $(this).offset().top-50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+      $(this).addClass('bgRLextend');
+    }else{
+      $(this).removeClass('bgRLextend');
+    }
+  });
+   // 文字列を囲う子要素
+  $('.bgappearTrigger').each(function(){ 
+    var elemPos = $(this).offset().top-50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+      $(this).addClass('bgappear');
+    }else{
+      $(this).removeClass('bgappear');
+    }
+  });   
+}
+
+// 画面をスクロールをしたら動かしたい場合に発動（簡易版）
+  $(window).scroll(function (){
+    BgFadeAnime();/* アニメーション用の関数を呼ぶ*/
+  });
+
+// 画面が読み込まれたらすぐに動かしたい場合に発動（簡易版）
+  $(window).on('load', function(){
+    BgFadeAnime();/* アニメーション用の関数を呼ぶ*/
   });
