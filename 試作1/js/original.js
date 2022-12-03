31e40a3a03af915f76e74534a4a037864eb47e29
Kazuki.Yamakawa <18236091@edu.cc.saga-u.ac.jp>
2022-12-04 06:59:36 +0900

    ベースと装飾の追加（service,guideline)

--- 試作1/js/original.js
+++ 試作1/js/original.js
@@ -27,6 +27,22 @@ $(window).scroll(function () {
     window.addEventListener('scroll', scrollAnimationFunc);
   });
 
+  $(window).scroll(function () {
+    var scrollAnimationElm = document.querySelectorAll('.scroll_up3');
+    var scrollAnimationFunc = function () {
+      for (var i = 0; i < scrollAnimationElm.length; i++) {
+        var triggerMargin = 20;
+//5pxだけ遅く表示
+        if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+          scrollAnimationElm[i].classList.add('show');
+        }
+      }
+    }
+    window.addEventListener('load', scrollAnimationFunc);
+    window.addEventListener('scroll', scrollAnimationFunc);
+  });
+
+
 
   // 背景色が伸びて出現（右から左）
   function BgFadeAnime(){
@@ -54,12 +70,55 @@ $(window).scroll(function () {
   });   
 }
 
+// 光るテキスト
+function GlowAnimeControl() {
+  $('.glowAnime').each(function () {
+    var elemPos = $(this).offset().top - 50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass("glow");
+
+    } else {
+      $(this).removeClass("glow");
+    }
+  });
+}
+
 // 画面をスクロールをしたら動かしたい場合に発動（簡易版）
   $(window).scroll(function (){
-    BgFadeAnime();/* アニメーション用の関数を呼ぶ*/
+    BgFadeAnime();
+    GlowAnimeControl();
   });
 
 // 画面が読み込まれたらすぐに動かしたい場合に発動（簡易版）
   $(window).on('load', function(){
-    BgFadeAnime();/* アニメーション用の関数を呼ぶ*/
+    BgFadeAnime();/*←これでアニメーション用の関数を呼ぶ。使い勝手いい。*/
   });
+
+
+//光るテキスト用の読み込み呼び出し（これは例外）
+$(window).on('load', function () {
+  //spanタグを追加する
+    var element = $(".glowAnime");
+  element.each(function () {
+    var text = $(this).text();
+    var textbox = "";
+    text.split('').forEach(function (t, i) {
+      if (t !== " ") {
+        if (i < 10) {
+          textbox += '<span style="animation-delay:.' + i + 's;">' + t + '</span>';
+        } else {
+        var n = i / 10;
+          textbox += '<span style="animation-delay:' + n + 's;">' + t + '</span>';
+        }
+
+      } else {
+        textbox += t;
+      }
+    });
+    $(this).html(textbox);
+  });
+
+  GlowAnimeControl();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
