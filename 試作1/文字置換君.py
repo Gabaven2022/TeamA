d4613df8a53a1ceaf5afb4ef3c71c7ab1b3dc63a
Kazuki.Yamakawa <18236091@edu.cc.saga-u.ac.jp>
2022-12-27 11:24:17 +0900

    編集済み削除

--- 試作1/文字置換君.py
+++ 試作1/文字置換君.py
@@ -1,11 +1,11 @@
-file_name="service3.html"
+file_name="faq.html"
 #上に置換したいファイル名を記入してね
 
 with open(file_name,encoding="UTF-8") as f:
     data_lines=f.read()
 
-data_lines=data_lines.replace("company","guideline")#("置換対象の文字","置換後の文字")
-data_lines=data_lines.replace("COMPANY","GUIDELINE")
+data_lines=data_lines.replace("編集済み","")#("置換対象の文字","置換後の文字")
+
 
 with open(file_name, mode="w", encoding="UTF-8") as f:
     f.write(data_lines)
